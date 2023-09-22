94598cd6f447bc232fb1c87ce59bba830bc20841
wolnik-kamil <112876137+wolnik-kamil@users.noreply.github.com>
2023-09-22 13:56:00 +0200

    Add files via upload

---
+++ Wolnik 4Ti/1sza lekcja z fromularzy/indexForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="indexForm.php">
+    <input type="text" name="a" required><br>
+    <input type="text" name="b" required><br>
+    <input type="submit" value="oblicz">
+    </form>
+</body>
+</html>
